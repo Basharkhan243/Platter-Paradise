a9d1257f9557b3c3c006bfe90833cca8fd0dcc74
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2025-01-01 22:08:35 +0530

    Delete MenuApp directory

--- MenuApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ImageData
-from .models import SearchQuery
-# Register your models here.
-
-admin.site.register(ImageData)
-admin.site.register(SearchQuery)
