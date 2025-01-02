207afc077d6bf4c897e51f384c3f0477f3e2d242
BASHAR AHMAD KHAN <9934809939khan@gmail.com>
2025-01-02 14:24:45 +0530

    Freeing space

---
+++ OneDrive/Documents/LANGUAGES/Javascript/index.js
@@ -0,0 +1,11 @@
+'use strict'
+/**
+ *    Preload
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preload]");
+window.addEventListener("load", function () {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
